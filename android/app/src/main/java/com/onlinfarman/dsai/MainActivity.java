dde6f49a4d8f4fd49dcef286079f9bd830db7b83
Farman safi <onlinfarman@gmail.com>
2026-02-07 01:11:16 +0500

    Added android platform files

---
+++ android/app/src/main/java/com/onlinfarman/dsai/MainActivity.java
@@ -0,0 +1,5 @@
+package com.onlinfarman.dsai;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.onlinfarman.dsai',
+  appName: 'DreamScene AI',
+  webDir: 'dist'
+};
+
+export default config;
